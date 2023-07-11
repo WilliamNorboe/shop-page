12ebf254deea26fba38bd9166c6a1807c9412746
William Norboe <william.norboe@gmail.com>
2023-07-11 10:51:40 -0700

    added hashrouter

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -2,15 +2,16 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import ShopPage from "./components/shop-page.js";
+import { HashRouter } from 'react-router-dom'
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/shop-page" element={<ShopPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
