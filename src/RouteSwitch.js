330b54fc78c1ac7435dc378f785633304137d122
William Norboe <william.norboe@gmail.com>
2023-08-30 07:36:02 -0700

    hashrouter

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,18 +1,34 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import ShopPage from "./components/shop-page.js";
-import { HashRouter} from 'react-router-dom'
+import { HashRouter, Routes, Route} from 'react-router-dom'
+// import {
+//   createHashRouter,
+//   RouterProvider
+// } from 'react-router-dom';
+
+// const router = createHashRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//   },
+//   {
+//     path: "/shop-page",
+//     element: <ShopPage />,
+//   }
+// ]);
 
 const RouteSwitch = () => {
   console.log("loaded");
   return (
-    <BrowserRouter basename={"/shopping-cart"}>
+    <HashRouter basename={"/"}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="shop-page" element={<ShopPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
+    // <RouterProvider router={router} />
   );
 };
 
